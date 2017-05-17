6196c37ae1f5abedd8b9d92db392395889f9cb76
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-05-17 17:55:02 +0100

    route action demo

--- app/routes/emberobject.js
+++ app/routes/emberobject.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    showAlert(data) {
+      alert(`Alert fired by action in ember object route with data from: ${data}`)
+    }
+  }
 });
--- tests/integration/components/ember-object-test.js
+++ tests/integration/components/ember-object-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ember-object', 'Integration | Component | ember object', {
+moduleForComponent('amazing-component', 'Integration | Component | ember object', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{ember-object}}`);
+  this.render(hbs`{{amazing-component}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#ember-object}}
+    {{#amazing-component}}
       template block text
-    {{/ember-object}}
+    {{/amazing-component}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
