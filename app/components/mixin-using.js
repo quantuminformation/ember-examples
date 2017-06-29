ceb12a2a43cc8bd30032904a1d2d39f82a350938
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-06-29 14:32:47 +0100

    More mixin examples

--- app/components/mixin-using.js
+++ app/components/mixin-using.js
@@ -3,9 +3,12 @@ import aMixin from '../mixins/aMixin';
 
 export default Ember.Component.extend(aMixin, {
 
-actions:{
-  update(message){
-    this.x2(message)
+  fooFromComponent: "Component prop",
+
+
+  actions: {
+    update(message){
+      this.x2(message)
+    }
   }
-}
 });
--- app/mixins/aMixin.js
+++ app/mixins/aMixin.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
 
 
+  fooFromMixin:"Sheesh kebabs",
+
+
   simple:Ember.inject.service(),
 
   x2(value){
